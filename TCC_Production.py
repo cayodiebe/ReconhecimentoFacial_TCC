65fba1c045eef2a2c297fdab21dc783c61f19392
= <cayodiebe@gmail.com>
2019-11-13 20:59:55 -0300

    First Commit

---
+++ TCC_Production.py
@@ -0,0 +1,38 @@
+import cv2
+import pymysql
+
+detectordeFaces = cv2.CascadeClassifier("haarcascade-frontalface-default.xml")
+reconhecedor = cv2.face.EigenFaceRecognizer_create()
+reconhecedor.read("classificadorEigen.yml")
+largura, altura = 220,220
+font = cv2.FONT_HERSHEY_COMPLEX_SMALL
+camera = cv2.VideoCapture(0)
+#camera = cv2.VideoCapture(0)
+conn = pymysql.connect(host='localhost', port=3306, user='root', passwd='', db='fitview')
+i = 0
+while (True):
+    conectado, imagem = camera.read()
+    imagemCinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
+    facesDetectadas = detectordeFaces.detectMultiScale(imagemCinza, scaleFactor=1.5, minSize=(30,30))
+
+    for(x,y,l,a) in facesDetectadas:
+        imagemFace = cv2.resize(imagemCinza[y:y + a, x:x + l], (largura, altura))
+        cv2.rectangle(imagem, (x,y), (x +l , y+a), (0,0,255), 2)
+        id, confianca = reconhecedor.predict(imagemFace)
+        nome = ""
+
+    cur = conn.cursor()
+    quantidade = len(facesDetectadas)
+    cv2.putText(imagem, str(quantidade), (10, 450), font, 3, (0, 255, 0), 2, cv2.LINE_AA)
+    while i>90:
+        cur.execute("""INSERT INTO Agente (data,contador) VALUES (now(), %s)""" % (quantidade))
+        conn.commit()
+        i=1
+    i = i+1
+    print(i)
+    cv2.imshow("Face", imagem)
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+camera.release()
+cv2.destroyAllWindows()
---
+++ app.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+from flask import Flask, render_template, Response
+import cv2
+import pymysql
+
+video = cv2.VideoCapture(0)
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+conn = pymysql.connect(host='localhost', port=3306, user='root', passwd='', db='fitview')
+classificador = cv2.CascadeClassifier('haarcascade-frontalface-default.xml')
+
+
+def gen():
+    while True:
+        rval, frame = video.read()
+        cv2.imwrite('t.jpg', frame)
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + open('t.jpg', 'rb').read() + b'\r\n')
+
+        cur = conn.cursor()
+
+        imagem = cv2.imread('t.jpg')
+
+        imagemcinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
+
+        deteccoes = classificador.detectMultiScale(imagemcinza, scaleFactor=1.1,
+                                                   minNeighbors=8,
+                                                   minSize=(30, 30),
+                                                   maxSize=(100, 100))
+
+
+        quantidade = len(deteccoes)
+
+        cur.execute("""INSERT INTO Agente (data,contador) VALUES (now(), %s)""" % (quantidade))
+
+        conn.commit()
+        cv2.destroyAllWindows()
+
+
+
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(),mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True, threaded=True)
